fa1ae44fb5a53daadd9b8767f92f059a00cf4027
Rubel Rudra <rubelrudra27@gmail.com>
2025-12-06 02:04:45 +0600

    Create useAPI and post data to DB

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import config from "./config";
-import initDB from "./config/db";
+import initDB, { pool } from "./config/db";
 
 const app = express();
 const port = config.port;
@@ -13,6 +13,28 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to vehicle rental management system !!");
 });
 
-app.listen(port, ()=>{
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email, password, phone, role } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name, email, password, phone, role) VALUES($1, $2, $3, $4, $5) RETURNING *`,
+      [name, email, password, phone, role]
+    );
+    const {created_at, updated_at, ...resData}= result.rows[0];
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: resData,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+app.listen(port, () => {
   console.log(`App listening on port ${port}`);
-})
+});
