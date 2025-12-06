3b4fa3509fa1970b0148881d9f61d6465063a55c
Rubel Rudra <rubelrudra27@gmail.com>
2025-12-06 14:04:24 +0600

    create moduler file and bcrypt pass

---
+++ src/modules/users/user.service.ts
@@ -0,0 +1,21 @@
+import bcrypt from "bcryptjs";
+import { pool } from "../../config/db";
+
+const createUser = async (payload: Record<string, unknown>) => {
+  const { name, email, password, phone, role } = payload;
+
+  const hashPassword = await bcrypt.hash(password as string, 10);
+
+  const result = await pool.query(
+    `INSERT INTO users(name, email, password, phone, role) VALUES($1, $2, $3, $4, $5) RETURNING *`,
+    [name, email, hashPassword, phone, role]
+  );
+  delete result.rows[0].password;
+  delete result.rows[0].created_at;
+  delete result.rows[0].updated_at;
+  return result;
+};
+
+export const userServices = {
+  createUser,
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import config from "./config";
 import initDB, { pool } from "./config/db";
+import { userRoutes } from "./modules/users/user.route";
 
 const app = express();
 const port = config.port;
@@ -13,27 +14,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to vehicle rental management system !!");
 });
 
-app.post("/users", async (req: Request, res: Response) => {
-  const { name, email, password, phone, role } = req.body;
-
-  try {
-    const result = await pool.query(
-      `INSERT INTO users(name, email, password, phone, role) VALUES($1, $2, $3, $4, $5) RETURNING *`,
-      [name, email, password, phone, role]
-    );
-    const {created_at, updated_at, ...resData}= result.rows[0];
-    res.status(201).json({
-      success: true,
-      message: "User registered successfully",
-      data: resData,
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message,
-    });
-  }
-});
+app.use("/api/v1/users", userRoutes);
 
 app.put("/users/:id", async(req:Request, res:Response)=>{
   const { name, email ,phone, role} = req.body;
