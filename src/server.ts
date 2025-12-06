3d8ab5c232fa53c983049f05f8822724f8640a1e
Rubel Rudra <rubelrudra27@gmail.com>
2025-12-06 12:12:26 +0600

    create a user

--- src/server.ts
+++ src/server.ts
@@ -35,6 +35,34 @@ app.post("/users", async (req: Request, res: Response) => {
   }
 });
 
+app.put("/users/:id", async(req:Request, res:Response)=>{
+  const { name, email ,phone, role} = req.body;
+
+  try {
+    const result = await pool.query(`UPDATE users SET name=$1, email=$2, phone=$3, role=$4, WHERE id=$5 RETURNING *`, [name,email,phone, role, req.params.id]);
+
+    if(result.rows.length === 0){
+      res.status(404).json({
+        success: false,
+        message: "User not found..."
+      })
+    }else{
+      res.status(200).json({
+        success: true,
+        message: "User updated successfully",
+        data: result.rows[0],
+      })
+    }
+
+  } catch (err:any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err
+    })
+  }
+})
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
