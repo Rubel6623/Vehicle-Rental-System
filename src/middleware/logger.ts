0f493a36269b1238160046602962f155ed542f1c
Rubel Rudra <rubelrudra27@gmail.com>
2025-12-10 15:09:00 +0600

    create a readme file

--- src/middleware/logger.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-//logger middleware
-const logger = (req: Request, res: Response, next: NextFunction) =>{
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
-  next();
-}
-
-export default logger;
